21b529071b9c6ab56458af04bdeed0d556cd9bb3
faroukali99 <[faroukoualiou2005@gmail.com]>
2026-01-30 01:18:43 +0100

    Initial commit

---
+++ reserveFound_service/src/main/java/com/lidcoin/reserveFound/ReserveFoundServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lidcoin.reserveFound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveFoundServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveFoundServiceApplication.class, args);
+	}
+
+}
---
+++ reserveFound_service/src/test/java/com/lidcoin/reserveFound/ReserveFoundServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lidcoin.reserveFound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveFoundServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
